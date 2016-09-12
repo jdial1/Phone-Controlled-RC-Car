f7c121a619f3ef64a124b80f5893bfffe7c749fd
Justin Dial <justinalandial@gmail.com>
2016-09-12 16:37:09 -0500

    Update Arduino RC Car.ino

--- Arduino/Arduino RC Car.ino
+++ Arduino/Arduino RC Car.ino
@@ -4,36 +4,36 @@ AF_DCMotor backmotor(4);
 AF_DCMotor frontmotor(1);
 
 void setup() {
-  pinMpode(A0, INPUT);//forward
-  pinMode(A1, INPUT);//backward
-  pinMode(A2, INPUT);//left
-  pinMode(A4, INPUT);//right
-
-  int voltCap=0;
-  int voltSum=0;
-  
+	pinMpode(A0, INPUT);//forward
+	pinMode(A1, INPUT);//backward
+	pinMode(A2, INPUT);//left
+	pinMode(A4, INPUT);//right
+	
+	int voltCap=0;
+	int voltSum=0;
+	
 	//get analogReads of each 4 10x
 	for (int i=0; i <= 10; i++){
 		  voltSum = voltSum +analogRead(A0)+analogRead(A1)+analogRead(A2)+analogRead(A4);
 		  delay(500);
-   } 
-   
-   //divide total by 40 to get average and add 50
-   voltCap=(voltAverage/40)+50;
-   
-   //set speeds of motors
+	} 
+	
+	//divide total by 40 to get average and add 50
+	voltCap=(voltAverage/40)+50;
+	
+	//set speeds of motors
 	backmotor.setSpeed(255);
 	frontmotor.setSpeed(150);
- 
-   //turn left twice to show setup is complete
-   frontmotor.run(BACKWARD);
-   delay(250);
-   backmotor.run(RELEASE);
-   delay(250);
-   backmotor.run(BACKWARD);
-   delay(250);
-   backmotor.run(RELEASE);
-   frontmotor.run(RELEASE);
+	
+	//turn left twice to show setup is complete
+	frontmotor.run(BACKWARD);
+	delay(250);
+	backmotor.run(RELEASE);
+	delay(250);
+	backmotor.run(BACKWARD);
+	delay(250);
+	backmotor.run(RELEASE);
+	frontmotor.run(RELEASE);
 }
 
 void loop() {
@@ -42,12 +42,12 @@ void loop() {
 	boolean Backward = false;
 	boolean Left = false;
 	boolean Right = false;
-
+	
 	if (analogRead(A0)>voltCap){Forward=true;}
 	if (analogRead(A1)>voltCap){Backward=true;}
 	if (analogRead(A2)>voltCap){Left=true;}
 	if (analogRead(A3)>voltCap){Right=true;}
- 
+	
 	//forward left
 	if (Forward&&Left){moveLeftForward();}
 	//forward right
@@ -68,22 +68,22 @@ void loop() {
 //forward methods
 void moveForward(){
 	backmotor.run(RELEASE);
-    delay(5);
-    backmotor.run(FORWARD);
+    	delay(5);
+	backmotor.run(FORWARD);
 }
 
 void moveLeftForward(){
 	turnLeft();
 	backmotor.run(RELEASE);
-    delay(5);
-    backmotor.run(FORWARD);
+	 delay(5);
+    	backmotor.run(FORWARD);
 }
 
 void moveRightForward(){
 	turnRight();
 	backmotor.run(RELEASE);
-    delay(5);
-    backmotor.run(FORWARD);
+	delay(5);
+	backmotor.run(FORWARD);
 }
 
 //backward methods
@@ -110,7 +110,7 @@ void moveRightBackward(){
 //turn methods
 void turnLeft(){
 	frontmotor.run(RELEASE);
-    delay(5);
+	delay(5);
 	frontmotor.run(BACKWARD);
 }
 
